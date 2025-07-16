ee743fa43354b60f5a46afa918e23e5e24b71ab3
Charles <adeakinyemi129@gmail.com>
2025-07-16 14:45:36 +0100

    implemented jwt strategy

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -6,15 +6,28 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
-  imports: [PrismaModule,
-    JwtModule.register({
-      secret: process.env.JWT_KEY,
-      signOptions: {expiresIn: '30m'},
-    })
+  imports: [PrismaModule, PassportModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async(configService: ConfigService) =>({
+        secret: configService.get<string>('JWT_KEY') || 'JWT_KEY',
+        signOptions: {expiresIn: '30m'}
+      }),
+      inject: [ConfigService]
+    }),
+    ConfigModule
+    // JwtModule.register({
+    //   secret: process.env.JWT_KEY,
+    //   signOptions: {expiresIn: '30m'},
+    // })
   ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
+  exports: [JwtStrategy, PassportModule]
 })
 export class AuthModule {}
